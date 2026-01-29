444dba81cafa567ebccfd3f0de318ab5a19c0531
Cynthia Okechukwu <cokecynthia4real2@yahoo.com>
2026-01-29 04:11:31 +0100

    Add comprehensive comments to all files

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 /**
  * Todo App Server Entry Point
  * Starts the Express server on the configured PORT
- * Default: http://localhost:5000
+ * Default: http://localhost:8080
  */
 
-const app = require('./src/app');
-const PORT = process.env.PORT || 5000;
+const app = require("./src/app");
+const PORT = process.env.PORT || 8080;
 
 // Start the server and listen for incoming requests
 app.listen(PORT, () => {
